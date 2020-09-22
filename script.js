2768a10517536dfd82740ff17c35b70bc2f79c87
GDSuperMoonX <gdsupermooncontact@gmail.com>
2020-09-22 15:13:01 +0000

    Added second styles in style.css because everyone can delete it in index.html and same with javascript.

---
+++ script.js
@@ -0,0 +1,11 @@
+      document.cookie = "main=true"
+      document.cookie = "html=5"
+      document.cookie = "css=4/3"
+      document.cookie = "javascript=ecmascript"
+      data = {
+        data: [
+          {
+            owner: "GDSuperMoon"
+          }
+        ]
+      }
